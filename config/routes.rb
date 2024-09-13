e6fa4b1aabf2206b4616f201c9d62249ecd50101
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-09-14 00:38:33 +0900

    日記一覧ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#top"
   resources :users, only: %i[new create]
+  resources :diaries, only: %i[index]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
