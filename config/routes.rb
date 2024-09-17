5825619fb413878f5f83e87c9a464b50e0bf9952
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-09-17 23:55:23 +0900

    編集、削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#top"
   resources :users, only: %i[new create]
-  resources :diaries, only: %i[index new create show]
+  resources :diaries, only: %i[index new create show edit destroy update]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
